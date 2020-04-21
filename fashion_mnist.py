ded62587cdf3f888ff9b4241b653474a4248dde6
Ravsehaj singh puri <ravsehaj@icloud.com>
2020-04-22 03:53:52 +0530

    Update fashion_mnist.py

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -1,3 +1,4 @@
+#--------------------------------------------IMPORTING NECESSARY PACKAGES AND LIBRARIES-------------------------------------#
 import keras
 from keras.layers import Conv2D, MaxPool2D, Flatten
 from keras.layers import Dense, Dropout
@@ -8,6 +9,7 @@ from google.colab.patches import cv2_imshow
 from scipy import ndimage
 import math
 
+#-------------------------LOADING DATASET,PREPROCESSING AND SPLITTING INTO TRAINING AND TESTING-----------------------------#
 fashion_mnist=keras.datasets.fashion_mnist
 (train_images,train_labels),(test_images,test_labels)=fashion_mnist.load_data()
 
@@ -21,6 +23,8 @@ test_images=test_images/255.0
 train_labels = keras.utils.np_utils.to_categorical(train_labels)
 test_labels = keras.utils.np_utils.to_categorical(test_labels)
 
+#------------------------------BUILDING CNN MODEL ARCHITECTURE USING SEQUENTIAL---------------------------------------------#
+
 model=keras.models.Sequential([
       keras.layers.Conv2D(32,(5,5),padding='same',input_shape=(28,28,1)),
       keras.layers.MaxPool2D((2,2)),
@@ -31,6 +35,8 @@ model=keras.models.Sequential([
       keras.layers.Dropout(0.5),
       keras.layers.Dense(10,activation='softmax')
 ])
+#--------------------------- TRAINING STARTS FROM HERE----------------------------------------------------------------------#
+
 model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
 model.fit(train_images,train_labels,epochs=25,verbose=2,batch_size=64)
 
@@ -41,64 +47,74 @@ predictions=model.predict(test_images)
 new_predictions=[]
 for i in range(len(test_labels)):
   new_predictions.append(np.argmax(predictions[i]))
+  
+'''
+UN-COMMENT THE BELOW BLOCK OF CODE TO TEST YOUR TRAINED MODEL ON SPECIFIC TEST IMAGES AND DISPLAYING THE RESULT
+'''
 
 #test_image_num=3000
 #plt.imshow(test_images[test_image_num])
 #print(class_names[new_predictions[test_image_num]])
 
-image_name='trouser.jpg'
-test_image=cv2.imread(image_name,cv2.IMREAD_GRAYSCALE)
-test_image=255-test_image
-test_image=cv2.resize(test_image,(28,28))
-(thresh, test_image) = cv2.threshold(test_image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+'''
+USE THE BELOW WRITTEN BLOCK OF CODE TO PREPROCESS ANY REAL WORLD FASHION IMAGE OF YOUR OWN AND MAKE PREDICTIONS BASED ON IT
+'''
 
-while np.sum(test_image[0])==0:
-  test_image=test_image[1:]
+def preprocess(image_path):
+  image_name=image_path
+  test_image=cv2.imread(image_name,cv2.IMREAD_GRAYSCALE)
+  test_image=255-test_image
+  test_image=cv2.resize(test_image,(28,28))
+  (thresh, test_image) = cv2.threshold(test_image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-while np.sum(test_image[:,0])==0:
-  test_image=np.delete(test_image,0,1)
+  while np.sum(test_image[0])==0:
+    test_image=test_image[1:]
 
-while np.sum(test_image[-1])==0:
-  test_image=test_image[:-1]
+  while np.sum(test_image[:,0])==0:
+    test_image=np.delete(test_image,0,1)
 
-while np.sum(test_image[:,-1])==0:
-  test_image=np.delete(test_image,-1,1)
+  while np.sum(test_image[-1])==0:
+    test_image=test_image[:-1]
 
-num_rows,num_cols=test_image.shape
+  while np.sum(test_image[:,-1])==0:
+    test_image=np.delete(test_image,-1,1)
 
-if num_rows>num_cols:
-  factor=20.0/num_rows
-  num_rows=20
-  num_cols=int(round(num_cols*factor))
-  test_image=cv2.resize(test_image,(num_cols,num_rows))
-else:
-  factor=20.0/num_cols
-  num_cols=20
-  num_rows=int(round(num_rows*factor))
-  test_image=cv2.resize(test_image,(num_cols,num_rows))
+  num_rows,num_cols=test_image.shape
 
-colPadding=(int(math.ceil((28-num_cols)/2.0)),int(math.floor((28-num_cols)/2.0)))
-rowPadding=(int(math.ceil((28-num_rows)/2.0)),int(math.floor((28-num_rows)/2.0)))
-test_image=np.lib.pad(test_image,(rowPadding,colPadding),'constant')
+  if num_rows>num_cols:
+    factor=20.0/num_rows
+    num_rows=20
+    num_cols=int(round(num_cols*factor))
+    test_image=cv2.resize(test_image,(num_cols,num_rows))
+  else:
+    factor=20.0/num_cols
+    num_cols=20
+    num_rows=int(round(num_rows*factor))
+    test_image=cv2.resize(test_image,(num_cols,num_rows))
+
+  colPadding=(int(math.ceil((28-num_cols)/2.0)),int(math.floor((28-num_cols)/2.0)))
+  rowPadding=(int(math.ceil((28-num_rows)/2.0)),int(math.floor((28-num_rows)/2.0)))
+  test_image=np.lib.pad(test_image,(rowPadding,colPadding),'constant')
 
 
-def calculate_shifts(image):
-  center_massx,center_massy=ndimage.measurements.center_of_mass(image)
-  num_rows,num_cols=image.shape
-  shiftX=np.round(num_cols/2.0-center_massx).astype(int)
-  shiftY=np.round(num_rows/2.0-center_massy).astype(int)
-  return shiftX,shiftY
+  def calculate_shifts(image):
+    center_massx,center_massy=ndimage.measurements.center_of_mass(image)
+    num_rows,num_cols=image.shape
+    shiftX=np.round(num_cols/2.0-center_massx).astype(int)
+    shiftY=np.round(num_rows/2.0-center_massy).astype(int)
+    return shiftX,shiftY
 
-def shift(image,sx,sy):
-  num_rows,num_cols=image.shape
-  N=np.float32([[1,0,sx],[0,1,sy]])
-  shifted_image=cv2.warpAffine(image,N,(num_rows,num_cols))
-  return shifted_image
+  def shift(image,sx,sy):
+    num_rows,num_cols=image.shape
+    N=np.float32([[1,0,sx],[0,1,sy]])
+    shifted_image=cv2.warpAffine(image,N,(num_rows,num_cols))
+    return shifted_image
 
-shiftX,shiftY=calculate_shifts(test_image)
-test_image=shift(test_image,shiftX,shiftY)
-
-test_image=test_image/255.0
+  shiftX,shiftY=calculate_shifts(test_image)
+  test_image=shift(test_image,shiftX,shiftY)
+  test_image=test_image/255.0
+  
+test_image=preprocess(None) # Add image path inplace of None
 plt.imshow(test_image)
 test_image=test_image.reshape(-1,28,28,1)
 print(class_names[np.argmax(model.predict(test_image))])
